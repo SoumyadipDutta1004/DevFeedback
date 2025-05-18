a5481dfc9eaead9fd540f561fb9657f4356bc2c6
SoumyadipDutta1004 <soumyadipdutta1004@gmail.com>
2025-05-18 21:35:45 +0530

    Initial backend setup

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { PORT } from './config/env';
+import connectDB from './database/mongoDB';
+
+
+const app = express();
+
+
+// Middlewares
+app.use(cors())
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+(async function() {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+  })
+})
